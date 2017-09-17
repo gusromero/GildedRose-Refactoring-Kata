59dcce287f6e1d4eaacc0d7868492fa5aade509f
gusromero <gusromero@gmail.com>
2017-09-17 19:07:42 +0200

    Extract method UpdateSellIn

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -25,7 +25,7 @@ namespace csharp
                 return;
             }
 
-            item.SellIn = item.SellIn - 1;
+            UpdateSellInSingleItem(item);
 
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
@@ -85,6 +85,10 @@ namespace csharp
                     }
                 }
             }
+        }
+
+        {
+            item.SellIn = item.SellIn - 1;
         }
     }
 }
