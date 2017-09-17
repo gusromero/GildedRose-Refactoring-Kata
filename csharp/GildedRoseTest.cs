474f8e4cac3175de20266045c0d45f1c276ee4d9
gusromero <gusromero@gmail.com>
2017-09-17 18:24:53 +0200

    Add TestSetup

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -6,13 +6,47 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
+        private IList<Item> _items;
+        private GildedRose _app;
+
+        [SetUp]
+        public void Setup()
+        {
+            _items = new List<Item> { };
+        }
         [Test]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("foo", Items[0].Name);
+            var item = new Item { Name = "foo", SellIn = 0, Quality = 0 } ;
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual("foo", _items[0].Name);
+        }
+
+        [Test]
+        public void SellInIsDecreased()
+        {
+            var item = new Item { Name = "foo", SellIn = 1, Quality = 1 };
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual(0, _items[0].SellIn);
+        }
+        [Test]
+        public void QualityIsDecreased()
+        {
+            var item = new Item { Name = "foo", SellIn = 1, Quality = 1 };
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual(0, _items[0].Quality);
         }
     }
 }
