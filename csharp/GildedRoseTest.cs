a657baeaf0d3bd7ef9c87d08f72ee3116ef35214
gusromero <gusromero@gmail.com>
2017-09-17 18:32:31 +0200

    Extract constants item names

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -9,6 +9,9 @@ namespace csharp
         private IList<Item> _items;
         private GildedRose _app;
 
+        private const string RandomItemName = "foo";
+        private const string AgedBrieItemName = "AgedBrie";
+
         [SetUp]
         public void Setup()
         {
@@ -17,19 +20,19 @@ namespace csharp
         [Test]
         public void foo()
         {
-            var item = new Item { Name = "foo", SellIn = 0, Quality = 0 } ;
+            var item = new Item { Name = RandomItemName, SellIn = 0, Quality = 0 } ;
             _items.Add(item);
             _app = new GildedRose(_items);
 
             _app.UpdateQuality();
 
-            Assert.AreEqual("foo", _items[0].Name);
+            Assert.AreEqual(RandomItemName, _items[0].Name);
         }
 
         [Test]
         public void SellInIsDecreased()
         {
-            var item = new Item { Name = "foo", SellIn = 1, Quality = 1 };
+            var item = new Item { Name = RandomItemName, SellIn = 1, Quality = 1 };
             _items.Add(item);
             _app = new GildedRose(_items);
 
@@ -37,16 +40,65 @@ namespace csharp
 
             Assert.AreEqual(0, _items[0].SellIn);
         }
+
         [Test]
         public void QualityIsDecreased()
         {
-            var item = new Item { Name = "foo", SellIn = 1, Quality = 1 };
+            var item = new Item { Name = RandomItemName, SellIn = 1, Quality = 1 };
             _items.Add(item);
             _app = new GildedRose(_items);
 
             _app.UpdateQuality();
 
             Assert.AreEqual(0, _items[0].Quality);
+        }
+
+        [Test]
+        public void AfterSellInDateQualityDecreasesTwice()
+        {
+            var item = new Item { Name = RandomItemName, SellIn = 0, Quality = 2 };
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual(0, _items[0].Quality);
+        }
+
+        [Test]
+        public void QualityIsNeverNegative()
+        {
+            var item = new Item { Name = RandomItemName, SellIn = 0, Quality = 0 };
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual(0, _items[0].Quality);
+        }
+
+        [Test]
+        public void BeforeSellInDateAgedBrieQualityIncreasesSimple()
+        {
+            var item = new Item { Name = AgedBrieItemName, SellIn = 1, Quality = 0 };
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual(1, _items[0].Quality);
+        }
+
+        [Test]
+        public void AfterSellInDateAgedBrieQualityIncreasesDouble()
+        {
+            var item = new Item { Name = AgedBrieItemName, SellIn = 0, Quality = 0 };
+            _items.Add(item);
+            _app = new GildedRose(_items);
+
+            _app.UpdateQuality();
+
+            Assert.AreEqual(2, _items[0].Quality);
         }
     }
 }
