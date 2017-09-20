4a78d3845371e42f4e511732f83f57aa8bbd093a
gusromero <gusromero@gmail.com>
2017-09-20 19:59:18 +0200

    Weird way to set a 0

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -79,7 +79,7 @@ namespace csharp
                     }
                     else
                     {
-                        item.Quality = item.Quality - item.Quality;
+                        item.Quality = 0;
                     }
                 }
                 else
